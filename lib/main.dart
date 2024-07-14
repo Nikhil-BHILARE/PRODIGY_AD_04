a98643e7a27f342f8a38f4b4b6e14fcd2e001680
Nikhil-BHILARE <nikhil.bhilare.20@gmail.com>
2024-07-14 23:04:49 +0530

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ttt_game/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Game',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
+    );
+  }
+}
